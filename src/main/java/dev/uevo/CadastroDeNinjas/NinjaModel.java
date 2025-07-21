ffcad11033c18c8b82e4ce68ddadda5612475282
simonjoseph <simaojose1978@gmail.com>
2025-07-21 10:16:18 +0100

    Ninja model and Ninja controller

---
+++ src/main/java/dev/uevo/CadastroDeNinjas/NinjaModel.java
@@ -0,0 +1,17 @@
+package dev.uevo.CadastroDeNinjas;
+
+public class NinjaModel {
+    
+    String nome;
+    int idade;
+    String email;
+
+    public NinjaModel() {
+    }
+    
+    public NinjaModel(String nome, int idade, String email) {
+        this.nome = nome;
+        this.idade = idade;
+        this.email = email;
+    }
+}
